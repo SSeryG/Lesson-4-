a7d8e3196bf9593693c05a56a9df68c20229747f
Vadim <vadim@.com>
2022-11-18 10:09:01 +0700

    PrimeGenerator.cs, версия 5 (неполная)

--- .vs/Les 5.1/5/5/PrimeGenerator.cs
+++ .vs/Les 5.1/5/5/PrimeGenerator.cs
@@ -51,4 +51,23 @@
     {
         return isCrossed[i] == false;
     }
+    private static void PutUncrossedIntegersIntoResult()
+    {
+        result = new int[NumberOfUncrossedIntegers()];
+        for (int j = 0, i = 2; i < isCrossed.Length; i++)
+        {
+            if (NotCrossed(i))
+                result[j++] = i;
+        }
+    }
+    private static int NumberOfUncrossedIntegers()
+    {
+        int count = 0;
+        for (int i = 2; i < isCrossed.Length; i++)
+        {
+            if (NotCrossed(i))
+                count++; // увеличить счетчик
+        }
+        return count;
+    }
 }
